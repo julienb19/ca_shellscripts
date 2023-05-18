d61054acf36a3c4b4a211a7c4cccb02e05a24bb2
julienb19 <132790572+julienb19@users.noreply.github.com>
2023-05-18 14:38:19 +0200

    Add files via upload

---
+++ variables.sh
@@ -0,0 +1,28 @@
+# !/bin/zsh
+
+BIRTHDATE="Jan 1, 2000"
+Presents=10
+BIRTHDAY=$(date -j -f "%b %d, %Y" "$BIRTHDATE" "+%A")
+
+echo "BIRTHDATE: $BIRTHDATE"
+echo "Presents: $Presents"
+echo "BIRTHDAY: $BIRTHDAY"
+
+if [ "$BIRTHDATE" == "Jan 1, 2000" ]; then
+    echo "BIRTHDATE is correct, it is $BIRTHDATE"
+else
+    echo "BIRTHDATE is incorrect - please retry"
+fi
+
+if [ $Presents -eq 10 ]; then
+    echo "I have received $Presents presents"
+else
+    echo "Presents is incorrect - please retry"
+fi
+
+if [ "$BIRTHDAY" == "Saturday" ]; then
+    echo "I was born on a $BIRTHDAY"
+else
+    echo "BIRTHDAY is incorrect - please retry"
+fi
+
